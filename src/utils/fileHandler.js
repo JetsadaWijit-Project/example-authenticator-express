6e8306128a6e0eb7bca863800176e9fe0aab2f42
JetsadaWijit <jetsadawijit@outlook.com>
2025-01-12 05:09:00 +0000

    Create ensureFileExists()

--- src/utils/fileHandler.js
+++ src/utils/fileHandler.js
@@ -6,8 +6,23 @@ const loadUsers = () => {
   return JSON.parse(fs.readFileSync(filePath, 'utf8') || '[]');
 };
 
+// Function to ensure the file exists
+function ensureFileExists(filePath, defaultContent = '[]') {
+  const dir = path.dirname(filePath);
+
+  // Create directories along the path if they don't exist
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+
+  // Create the file if it doesn't exist
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, defaultContent, 'utf8');
+  }
+}
+
 const saveUsers = (users) => {
   fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
 };
 
-module.exports = { loadUsers, saveUsers };
+module.exports = { ensureFileExists, loadUsers, saveUsers };
